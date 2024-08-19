38aeb46314ed910ae996b58c9be8b9f396cfb39e
COMPUTER WALA <simranmansoori1996@gmail.com>
2024-08-19 15:41:13 -0700

    modified footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
    const footerHTML =  `<footer class="section-footer">
         <div class="footer-container container">
           <div class="content_1">
-            <img src="./images/logo.png" alt="logo" />
+            <img src="./public/images/logo.png" alt="logo" />
             <p>
               Welcome to Thapa EcomStore, your ultimate destination for
               cutting-edge gadgets!
